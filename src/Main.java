00fba46a69e6cbb5275f0dc0c39ce4c4cffc7156
Aldan Paul <paula0628@students.bowiestate.edu>
2026-09-17 10:22:10 -0400

    Constructor Inheritance

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//
 public class Main {
     public static void main(String[] args) {
     //Create two person object
 
         Person p1=new Person();
 
-        Person p2=new Person(72,19,"Blue","Ashley Andrews",139);
+//        Person p2=new Person(72,19,"Blue","Ashley Andrews",139);
+//        System.out.println(p2.height);
+
+        Person p2=new Person(72,19);
         System.out.println(p2.height);
         System.out.println(p2.age);
         System.out.println(p2.color);
         System.out.println(p2.name);
         System.out.println(p2.weight);
+
+        Person p3=p2.Call_Private();
+
+
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -8,7 +8,8 @@ public class Person
     double weight;
 
     //Default Constructor
-    Person()
+    //Constructors can have private, protected, or public access modifiers
+    public Person()
     {
         height=75;
         age=21;
@@ -16,8 +17,14 @@ public class Person
         name="";
         weight=152;
     }
+    //Protected members are accessible within the same package - java files under same directory/folder
+    protected Person(double height, int age)
+    {
+        this.height=height;
+        this.age=age;
+    }
     //Overloaded Constructor
-    Person(double height, int age, String color, String name, double weight)
+    private Person(double height, int age, String color, String name, double weight)
     //Parameters are local variables to the constructor
     {
         this.height=height;
@@ -25,5 +32,10 @@ public class Person
         this.color=color;
         this.name=name;
         this.weight=weight;
+    }
+    public Person Call_Private()
+    {
+        Person p3=new Person(72, 19, "red", "Paul Gills", 130);
+        return p3;
     }
 }
